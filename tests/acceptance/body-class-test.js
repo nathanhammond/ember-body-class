ec283a1b2652e7f563a748f4ffa98a80135650c1
Nathan Hammond <github.com@nathanhammond.com>
2018-08-02 12:00:54 -0700

    Reword acceptance tests.

--- tests/acceptance/body-class-test.js
+++ tests/acceptance/body-class-test.js
@@ -7,26 +7,17 @@ module('Acceptance | ember body class', function(hooks) {
 
   test('visiting /', async function(assert) {
     await visit('/');
-
     assert.equal(currentURL(), '/');
 
-    /* bodyClass set in dummy application route */
-    assert.ok(document.body.classList.contains('yolo'), "yolo class is missing");
+    assert.ok(document.body.classList.contains('yolo'), 'yolo class is set.');
   });
 
   test('visiting /test', async function(assert) {
     await visit('/test');
-
     assert.equal(currentURL(), '/test');
 
-    /* bodyClass set in dummy application route */
-    assert.ok(document.body.classList.contains('yolo'), "yolo class is missing");
-    assert.ok(document.body.classList.contains('another-yolo'), "another-yolo class is missing");
+    assert.ok(document.body.classList.contains('yolo'), 'yolo class is set from application route.');
+    assert.ok(document.body.classList.contains('another-yolo'), 'another-yolo class is set from test route.');
+    assert.notOk(document.body.classList.contains('null'), 'body has no null class');
   });
-
-  test('make sure there are no null values', async function(assert) {
-    await visit('/test');
-
-    assert.ok(!document.body.classList.contains('null'), "body has null class");
-  })
 });
